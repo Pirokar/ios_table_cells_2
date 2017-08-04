c6764071314181ce70de5e5a7f0d988727376752
Vladimir Rybalka <vladimir@Vladimirs-Mac.local>
2017-08-04 16:43:57 +0300

    implemented custom cell with code way

---
+++ Table Cells/NameAndColorTableViewCell.swift
@@ -0,0 +1,58 @@
+//
+//  NameAndColorTableViewCell.swift
+//  Table Cells
+//
+//  Created by Vladimir Rybalka on 04/08/2017.
+//  Copyright © 2017 Vladimir Rybalka. All rights reserved.
+//
+
+import UIKit
+
+class NameAndColorTableViewCell: UITableViewCell {
+    var name: String = "" {
+        didSet {
+            if(name != oldValue) {
+                nameLabel.text = name
+            }
+        }
+    }
+    var color: String = "" {
+        didSet {
+            if(color != oldValue) {
+                colorLabel.text = color
+            }
+        }
+    }
+    var nameLabel: UILabel!
+    var colorLabel: UILabel!
+
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        let nameLabelRect = CGRect(x: 0, y: 5, width: 70, height: 15)
+        let nameMarker = UILabel(frame: nameLabelRect)
+        nameMarker.textAlignment = NSTextAlignment.right
+        nameMarker.text = "Имя:"
+        nameMarker.font = UIFont.boldSystemFont(ofSize: 12)
+        contentView.addSubview(nameMarker)
+
+        let colorLabelRect = CGRect(x: 0, y: 26, width: 70, height: 15)
+        let colorMarker = UILabel(frame: colorLabelRect)
+        colorMarker.textAlignment = NSTextAlignment.right
+        colorMarker.text = "Цвет:"
+        colorMarker.font = UIFont.boldSystemFont(ofSize: 12)
+        contentView.addSubview(colorMarker)
+        
+        let nameValueRect = CGRect(x: 80, y: 5, width: 200, height: 15)
+        nameLabel = UILabel(frame: nameValueRect)
+        contentView.addSubview(nameLabel)
+        
+        let colorValueRect = CGRect(x: 80, y: 25, width: 200, height: 15)
+        colorLabel = UILabel(frame: colorValueRect)
+        contentView.addSubview(colorLabel)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- Table Cells/ViewController.swift
+++ Table Cells/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    let cellTableIdentifier = "CellTableIdentifier"
+    @IBOutlet var tableView: UITableView!
+    
+    let computers = [
+        ["Name" : "MacBook Air", "Color" : "Silver"],
+        ["Name" : "MacBook Pro", "Color" : "Silver"],
+        ["Name" : "iMac", "Color" : "Silver"],
+        ["Name" : "Mac Mini", "Color" : "Silver"],
+        ["Name" : "Mac Pro", "Color" : "Black"],
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.register(NameAndColorTableViewCell.self, forCellReuseIdentifier: cellTableIdentifier)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return computers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellTableIdentifier, for: indexPath) as! NameAndColorTableViewCell
+        
+        let rowData = computers[indexPath.row]
+        cell.name = rowData["Name"]!
+        cell.color = rowData["Color"]!
+        
+        return cell
+    }
 }
 
